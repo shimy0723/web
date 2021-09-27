dedca07f79fc892ff0a569a78483078cdc8a454a
shimy0723 <shimy0723@gmail.com>
2021-09-28 06:15:46 +1300

    navigation translate updated

--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,32 @@
-// Menu Button 
+// Menu button 
 const menuButton = document.querySelector("#open-menu");
 // Navigation
 const nav = document.querySelector("#nav");
-// Menu Closed
-let menuOpen = false;
 // Total wrapper div
 const totalWrapperDiv = document.querySelector("#total-wrapper");
+// Condition of use link
+const condition = document.querySelector("#condition-of-use-link");
+// Signature div
+const signature = document.querySelector("#signature");
 
+// Menu Closed
+let menuOpen = false;
 // Menu Button Click
 menuButton.addEventListener('click', ()=> {
-    if(!menuOpen){
-        menuButton.classList.add('open');
+    if(!menuOpen){        
+        menuButton.classList.add('opened');
         nav.classList.add('visible');
-        totalWrapperDiv.classList.add('open');
-        menuOpen = true;
-    } else {
-        menuButton.classList.remove('open');
+        totalWrapperDiv.classList.add('opened');
+        condition.classList.add('opened');
+        signature.classList.add('opened');   
+        menuOpen = true;    
+    } else {        
+        menuButton.classList.remove('opened');
         nav.classList.remove('visible');
-        totalWrapperDiv.classList.remove('open');
-        menuOpen = false;
-    }
+        totalWrapperDiv.classList.remove('opened');
+        condition.classList.remove('opened');
+        signature.classList.remove('opened');    
+        menuOpen = false;    
+    }    
 });
 

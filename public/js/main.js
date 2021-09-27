f5f006acaf08338e5e54a76335205d087eaae992
shimy0723 <shimy0723@gmail.com>
2021-09-28 04:48:57 +1300

    latest update; open nav button transition updated; total wrapper matrix3d updated

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,24 @@
-const menuButton = document.querySelector(".menu-button");
+// Menu Button 
+const menuButton = document.querySelector("#open-menu");
+// Navigation
+const nav = document.querySelector("#nav");
+// Menu Closed
 let menuOpen = false;
+// Total wrapper div
+const totalWrapperDiv = document.querySelector("#total-wrapper");
+
+// Menu Button Click
 menuButton.addEventListener('click', ()=> {
     if(!menuOpen){
         menuButton.classList.add('open');
+        nav.classList.add('visible');
+        totalWrapperDiv.classList.add('open');
         menuOpen = true;
     } else {
         menuButton.classList.remove('open');
+        nav.classList.remove('visible');
+        totalWrapperDiv.classList.remove('open');
         menuOpen = false;
     }
 });
+
